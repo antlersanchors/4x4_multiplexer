e3d6e0ff8299d51d05c370340553a81e76215b42
antlersanchors <michaelowen.liston@gmail.com>
2015-03-19 18:47:25 +0100

    Cleaned up starting arrays, removing top four pins from each

--- 4x4_multiplexer.ino
+++ 4x4_multiplexer.ino
@@ -36,11 +36,11 @@
 
 // 2-dimensional array of row pin numbers:
 const int row[4] = {
-  2,7,19,5,13,18,12,16 };
+  2,7,5,12 };
 
 // 2-dimensional array of column pin numbers:
 const int col[4] = {
-  6,11,10,3,17,4,4,9  };
+  6,3,4,8 };
 
 // 2-dimensional array of pixels:
 int pixels[4][4];          
